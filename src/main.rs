c201bb7f3866a447faa99e85b1640d2f905350be
Ismail Salah Mahdi <ismail.merced3@gmail.com>
2025-10-24 16:30:30 +0300

    format

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,13 @@ struct Cli {
     /// ادخل النمط المراد البحث عنه
     pattern: String,
     /// ادخل المسار المراد البحث فيه
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("لا يمكن قراءة الملف {}", args.path.display()))?;
-
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
